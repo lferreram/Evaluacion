7bd5a4612841d6622403db6cf1be05a205fd862e
lferreram <lferreram@yahoo.com>
2023-05-31 20:52:34 -0600

    first commit

---
+++ Ejercicio3.js
@@ -0,0 +1,6 @@
+/*Cree un programa en js, donde se muestre la tabla de multiplicar de la tabla de 7.  
+Como se muestre a continuación: (25 puntos)*/
+
+let numero=prompt("Ingrese la tabla que desea multiplicar")
+for (let  i=0; i<=10;i++)
+{console.log(`${numero}*${i} = ${numero*i}`)}
